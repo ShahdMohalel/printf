8a880f0d4f41f6173ebc151d9c6c3088c2b7bd83
mayaassem5 <mayaassem5@gmail.com>
2023-10-15 13:39:15 -0700

    edit

--- _printf.c
+++ _printf.c
@@ -11,40 +11,40 @@
  */
 int _printf(const char *format, ...)
 {
-	int i;
     int len = 0;
     va_list ptr;
     va_start(ptr, format);
 
-    for (i = 0; format[i] != '\0'; i++)
+    while(*format != '\0')
     {
-        if (format[i] == '%')
-        {
-            if (format[i + 1] == 'c')
-            {
-                int c = va_arg(ptr, int);
-                len += _putchar(c);
-                i++;
-            }
-            else if (format[i + 1] == 's')
-            {
-                char *s = va_arg(ptr, char *);
-                while (*s != '\0')
-                {
-                    len += _putchar(*s);
-                    s++;
-                }
-                i++;
-            }
-            else if (format[i + 1] == '%')
-            {
-                len += _putchar('%');
-                i++;
-            }
-        }
-        else
-        {
-            len += _putchar(format[i]);
+	    format++;
+	    switch(*format)
+	    {
+		    case 'c':
+	                int c = va_arg(ptr, int);
+        	        len += _putchar(c);
+               		format++;
+			break;
+
+		    case 's':
+	                char *s = va_arg(ptr, char *);
+        	        while (*s != '\0')
+               	 	{
+				 len += _putchar(*s);
+				s++;
+                	}
+                	format++;
+			break;
+
+		case '%':
+
+               		 len += _putchar('%');
+                	format++;
+			break;
+
+		default:
+           		 len += _putchar(format[i]);
+			 break;
         }
     }
 
