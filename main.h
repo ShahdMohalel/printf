9a617487c49d75c267a19378f1f744fae05cc52c
mayaassem5 <mayaassem5@gmail.com>
2023-10-15 13:21:06 -0700

    edit

--- main.h
+++ main.h
@@ -1,11 +1,7 @@
 #ifndef MAIN_H
 #define MAIN_H
 
-#include <stdio.h>
-#include <stdarg.h>
-#include <unistd.h>
 
-int switcher(const char *format, va_list args, int len);
 int _putchar(char c);
 
 int _printf(const char *format, ...);
