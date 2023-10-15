9f8e2647e9dfa73f2288aa87608930ece05047fc
mayaassem5 <mayaassem5@gmail.com>
2023-10-15 14:26:20 -0700

    edit

--- _printf.c
+++ _printf.c
@@ -15,6 +15,12 @@ int _printf(const char *format, ...)
     va_list ptr;
     va_start(ptr, format);
 
+    if(!format || (format[0] == '%' && !format[1]))
+	    return (-1);
+
+    if(format[0] == '%' && format[1] == ' ' && !format[2])
+	    return (-1);
+
     while (*format != '\0')
     {
         if (*format == '%')
