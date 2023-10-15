136803f1894962b352d50c4af69dc0587e68bd5e
ShahdMohalel <shahd.mohlal@gmail.com>
2023-10-15 11:43:25 -0700

    gg

--- _printf.c
+++ _printf.c
@@ -10,51 +10,43 @@
  */
 int _printf(const char *format, ...)
 {
-int i;
-int j;
-int len = 0;
-va_list ptr;
-if (format == NULL)
-{
-return (0);
-}
-va_start(ptr, format);
-for (i = 0; format[i] != '\0'; i++)
-{
-char *s;
-if (format[i] == '%')
-{
-if (format[i + 1] == 'c')
-{
-_putchar(va_arg(ptr, int));
-len++;
-} 
-else if (format[i + 1] == 's')
-{
-s = va_arg(ptr, char *);
-if(*s != '\0')
-{
-for (j = 0; s[j] != '\0'; j++)
-{
-_putchar(s[j]);
-len++;
-}
-}
-} 
-else if (format[i + 1] == '%')
-{
-_putchar('%');
-len++;
-}
-if (format[i] == '%' && (format[i + 1] == 'c' || format[i + 1] == 's'))
-{
-i++;
-continue;
-}
-}
-_putchar(format[i]);
-len++;
-}
-va_end(ptr);
-return (len);
+    int len = 0;
+    int i;
+    va_list ptr;
+    va_start(ptr, format);
+
+    for (i = 0; format[i] != '\0'; i++)
+    {
+        if (format[i] == '%')
+        {
+            if (format[i + 1] == 'c')
+            {
+                int c = va_arg(ptr, int);
+                len += _putchar(c);
+                i++;
+            }
+            else if (format[i + 1] == 's')
+            {
+                char *s = va_arg(ptr, char *);
+                while (*s != '\0')
+                {
+                    len += _putchar(*s);
+                    s++;
+                }
+                i++;
+            }
+            else if (format[i + 1] == '%')
+            {
+                len += _putchar('%');
+                i++;
+            }
+        }
+        else
+        {
+            len += _putchar(format[i]);
+        }
+    }
+
+    va_end(ptr);
+    return len;
 }
