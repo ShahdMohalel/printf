1a4db48c126dd24a5f0c26590c29633d87489705
mayaassem5 <mayaassem5@gmail.com>
2023-10-15 15:11:00 -0700

    edit

--- _printf.c
+++ _printf.c
@@ -18,9 +18,6 @@ int _printf(const char *format, ...)
     if(!format || (format[0] == '%' && !format[1]))
 	    return (-1);
 
-    if(format[0] == '%' && format[1] == ' ' && !format[2])
-	    return (-1);
-
     while (*format != '\0')
     {
 
