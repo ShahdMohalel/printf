044c984531b8c77f15bc5602c705a7160411949d
mayaassem5 <mayaassem5@gmail.com>
2023-10-16 05:21:02 -0700

    edit

--- printf_string.c
+++ printf_string.c
@@ -11,6 +11,9 @@ int printf_string(va_list args, int len)
 {
 	char *string = va_arg(args, char *);
 
+	if(string == '\0')
+		string = "(null)";
+
 	while (*string != '\0')
 	{
 		_putchar(*string);
