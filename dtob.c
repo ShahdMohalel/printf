edb37652f28b6816d4220add7361c6b2de7b3145
mayaassem5 <mayaassem5@gmail.com>
2023-10-16 12:09:35 -0700

    edit

--- dtob.c
+++ dtob.c
@@ -9,43 +9,29 @@
  */
 int dtob(unsigned long int n, int len)
 {
-	unsigned long int binary[100] = {0};
-	int i = 0;
+    if (n == 0)
+    {
+        print_char('0');
+        len++;
+        return len;
+    }
 
-	/*if (n == INT_MAX)
-	{
-		int max_digits[32];
-    		for (i = 0; i < 32; i++)
-    		{
-        		max_digits[i] = 1;
-    		}
-		for (i = 31; i >= 0; i--)
-    		{
-        		print_char(max_digits[i] + '0');
-    		}
-    		len += 32;
-	}*/
+    int binary[64];  // Use a larger array to accommodate long values
+    int i = 0;
 
-	if (n == 0)
-	{
-		print_char('0');
-		len++;
-		return (len);
-	}
+    while (n > 0)
+    {
+        binary[i] = n % 2;
+        n /= 2;
+        i++;
+    }
 
-	while (n > 0)
-	{
-		binary[i] = n % 2;
-		n /= 2;
-		i++;
-	}
+    while (i > 0)
+    {
+        i--;
+        print_char('0' + binary[i]);
+        len++;
+    }
 
-	while (i > 0)
-	{
-		i--;
-		print_char('0' + binary[i]);
-		len++;
-	}
-
-	return (len);
+    return len;
 }
