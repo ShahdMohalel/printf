a905137c97e4043101ae288c1de8207a13360952
ShahdMohalel <shahd.mohlal@gmail.com>
2023-10-17 15:07:59 -0700

    gg

--- rev.c
+++ rev.c
@@ -6,23 +6,17 @@
  */
 int rev(va_list args)
 {
-    int i;
-    int count = 0;
-    char *str;
-    str = va_arg(args, char *);
-    if (str == NULL)
-    {
-        return (0);
-    }
-    
-    for(i = 0; str[i] != '\0'; i++)
-    {
-        count++;
-    }
-    for (i = (count - 1); i >= 0; i--)
-    {
-        _putchar(str[i]);
-        
-    }
-    return (count);
+    int i = 0, j;
+    char *s = va_arg(args, char *);
+
+    if (!s)
+        s = "(null)";
+
+    while (s[i])
+        i++;
+
+    for (j = i - 1; j >= 0; j--)
+        _putchar(s[j]);
+
+    return (i);
 }
