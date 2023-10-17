eaa8f456d8ff29523f8b193bfe15cde6919ddcf9
mayaassem5 <mayaassem5@gmail.com>
2023-10-17 03:07:10 -0700

    edit

--- get_func.c
+++ get_func.c
@@ -2,20 +2,15 @@
 
 /**
  * get_print - selects the right printing function
- * depending on the conversion specifier passed to _printf
  * @s: character that holds the conversion specifier
- * Description: the function loops through the structs array
- * func_arr[] to find a match between the specifier passed to _printf
- * and the first element of the struct, and then the approriate
- * printing function
  * Return: a pointer to the matching printing function
  */
 int (*get_func(char s))(va_list, flags_t *)
 {
     int i, flags = 14;
-    
+
 	func_t func_arr[] = {
-		{'s', print_string},
+		{'s', print_str},
 		{'c', print_char}
 		};
 
