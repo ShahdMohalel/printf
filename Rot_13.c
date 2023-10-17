0c200151a919b0f2058fa4e1be2594cb5cfd558f
ShahdMohalel <shahd.mohlal@gmail.com>
2023-10-17 14:41:45 -0700

    gg

---
+++ Rot_13.c
@@ -0,0 +1,39 @@
+#include "main.h"
+/**
+*print_rot13 - prints a string using rot13
+*@args: args
+*Return: length
+*/
+#include "main.h"
+
+/**
+ * print_rot13 - prints a string using rot13
+ * @args: args
+ * Return: length
+ */
+
+int print_rot13(va_list args)
+{
+    int i, x;
+    char *str;
+    char s[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+    char u[] = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
+
+    str = va_arg(args, char *);
+    if (str == NULL)
+        return (-1);
+    for (i = 0; str[i] != '\0'; i++)
+    {
+        for (x = 0; x <= 52; x++)
+        {
+            if (str[i] == s[x])
+            {
+                _putchar(u[x]);
+                break;
+            }
+        }
+        if (x == 53)
+            _putchar(str[i]);
+    }
+    return (i);
+}
