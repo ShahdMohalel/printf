2b1d90fe04de337e31b597fc4a7bb1c3df499a5d
ShahdMohalel <shahd.mohlal@gmail.com>
2023-10-17 15:50:15 -0700

    hh

--- space.c
+++ space.c
@@ -6,21 +6,21 @@
  */
 int space(va_list args)
 {
-    int len = 0;
-    int num;
-    num = va_arg(args, int);
-    if (num < 0)
-    {
-        putchar('-');
-        len++;
-        num = num / -1;
-        len += _print_int(num);
-    }
-    else
-    {
-        putchar(' ');
-        len++;
-        len += _print_int(num);
-    }
-    return (len);
+int len = 0;
+int num;
+num = va_arg(args, int);
+if (num < 0)
+{
+putchar('-');
+len++;
+num = (num / -1);
+len += _print_int(num);
+}
+else
+{
+_putchar('\t');
+len++;
+len += _print_int(num);
+}
+return (len);
 }
