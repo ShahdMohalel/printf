546e567fee66e26d5a82c9896f90a509b1d5cff4
ShahdMohalel <shahd.mohlal@gmail.com>
2023-10-17 15:54:57 -0700

    ggg

--- Rot_13.c
+++ Rot_13.c
@@ -7,27 +7,25 @@
 
 int print_rot13(va_list args)
 {
-    int i, x;
-    char *str;
-    char s[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
-    char u[] = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
-
-    str = va_arg(args, char *);
-    if (str == NULL)
-        return (-1);
-    for (i = 0; str[i] != '\0'; i++)
-    {
-        for (x = 0; x < 52; x++)
-        {
-            if (str[i] == s[x])
-            {
-                _putchar(u[x]);
-                break;
-            }
-        }
-        if (x == 52)
-            _putchar(str[i]);
-    }
-    return (i);
+int i, x;
+char *str;
+char s[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+char u[] = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
+str = va_arg(args, char *);
+if (str == NULL)
+return (-1);
+for (i = 0; str[i] != '\0'; i++)
+{
+for (x = 0; x < 52; x++)
+{
+if (str[i] == s[x])
+{
+_putchar(u[x]);
+break;
 }
-
+}
+if (x == 52)
+_putchar(str[i]);
+}
+return (i);
+}
