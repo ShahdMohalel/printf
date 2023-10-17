c2556109fcb19f7fc44baf211d432783b21bac68
ShahdMohalel <shahd.mohlal@gmail.com>
2023-10-17 16:01:24 -0700

    hh

--- plus.c
+++ plus.c
@@ -6,26 +6,26 @@
  */
 int plus(va_list args)
 {
-    int len = 0;
-    int number;
-    number = va_arg(args, int);
-    if (number > 0)
-    {
-        putchar('+');
-        len = _print_int(number);
-        len++;
-    }
-    if (number < 0)
-    {
-        number = number / -1;
-        putchar('-');
-        len = _print_int(number);
-        len++;
-    }
-    if (number == 0)
-    {
-        putchar('0');
-        len = 1;
-    }
-    return (len);
+int len = 0;
+int number;
+number = va_arg(args, int);
+if (number > 0)
+{
+_putchar('+');
+len = _print_int(number);
+len++;
+}
+if (number < 0)
+{
+number = number / -1;
+_putchar('-');
+len = _print_int(number);
+len++;
+}
+if (number == 0)
+{
+_putchar('0');
+len = 1;
+}
+return (len);
 }
