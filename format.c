b962c27b5066d8f744d75a5419ab18cc4e267e1a
ShahdMohalel <shahd.mohlal@gmail.com>
2023-10-17 17:07:57 -0700

    gg

--- format.c
+++ format.c
@@ -12,7 +12,7 @@ int format_reciever(const char *format, conver_t f_list[], va_list arg_list)
 int i, j, r_val, len;
 len = 0;
 for (i = 0; format[i] != '\0'; i++)
-	{
+{
 		if (format[i] == '%')
 		{
 			for (j = 0; f_list[j].sym != NULL; j++)
